d17fd07aa5850300bf3d5b12757eb30782f443a4
Seba Avila <avila.sebastianm@gmail.com>
2024-05-29 10:51:41 -0300

    578923

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -1,24 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {CounterComponent} from "./counter/counter.component";
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+
+import {CounterModule} from "./counter/counter.module";
+import {HeroesModule} from "./heroes/heroes.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {MainPageComponent} from "./dbz/pages/main-page.component";
+import {DbzModule} from "./dbz/dbz.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent,
+
+
 
   ],
   imports: [
+    HeroesModule,
+    CounterModule,
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    DbzModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
