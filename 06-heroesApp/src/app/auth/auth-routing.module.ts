e8d1f145c83ef8fdadeb2762f95cb4355591ebb6
Seba-Archlinux <avila.sebastianm@gmail.com>
2024-09-10 11:07:34 -0300

    75

--- 06-heroesApp/src/app/auth/auth-routing.module.ts
+++ 06-heroesApp/src/app/auth/auth-routing.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from "./pages/layout/layout.component";
+import {RegisterPageComponent} from "./pages/register-page/register-page.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginPageComponent
+      },
+      {
+        path: 'new-account',
+        component: RegisterPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+}
--- 06-heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -1,11 +1,47 @@
 import {Routes, RouterModule} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {LayoutPageComponent} from "./pages/layout-page/layout-page.component";
+import {ListPageComponent} from "./pages/list-page/list-page.component";
+import {NewPageComponent} from "./pages/new-page/new-page.component";
+import {ShearchPageComponent} from "./pages/search-page/shearch-page.component";
+import {HeroPageComponent} from "./pages/hero-page/hero-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      {
+        path: 'new-hero',
+        component: NewPageComponent
+      },
+      {
+        path: 'search',
+        component: ShearchPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: ':id',
+        component: HeroPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class HeroesRoutingModule {
