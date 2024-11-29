b44f1d1e70e0800acd2eec1534c9f2447a29cb0e
Seba-Archlinux <avila.sebastianm@gmail.com>
2024-11-29 07:11:22 -0300

    46512

--- 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {AuthService} from "../../../auth/services/auth.service";
+import {Router} from "@angular/router";
+import {User} from "../../../auth/interfaces/user.interface";
 
 @Component({
   selector: 'app-layout-page',
@@ -11,5 +14,18 @@ export class LayoutPageComponent {
     {label: 'Anadir', icon: 'add', url: './new-hero'},
     {label: 'Buscador', icon: 'search', url: './search'}
   ]
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+  }
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout(),
+      this.router.navigate(['auth/login'])
+  }
 
 }
